7beba20bd6667aaecf9ada4bedde7dd4b0501235
codemaestro02 <maadeleke02@gmail.com>
2024-12-12 13:14:33 +0100

    Added permission to flight creation

--- flights/urls.py
+++ flights/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AdminLoginView, AdminRegisterView, FlightPackageView
+from .views import AdminLoginView, AdminRegisterView, FlightPackageView, FlightPackageCreateView
 
 urlpatterns = [
     path('admin/register', AdminRegisterView.as_view(), name='admin_register'),
     path('admin/login/', AdminLoginView.as_view(), name='admin_login'),
-    path('flight/packages/', FlightPackageView.as_view(), name='list_create_packages'),
+    path('flight/package/', FlightPackageCreateView.as_view(), name='create_package'),
+    path('flight/packages/', FlightPackageView.as_view(), name='list_packages'),
     path('flight/package/<int:pk>/', FlightPackageView.as_view(), name='retrieve_update_delete_package'),
 ]
--- flights/views.py
+++ flights/views.py
@@ -36,8 +36,19 @@ class AdminLoginView(APIView):
             return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class FlightPackageView(APIView):
+class FlightPackageCreateView(APIView):
     permission_classes = [IsAuthenticated]
+    serializer_class = FlightPackageSerializer
+
+    def post(self, request):
+        serializer = FlightPackageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class FlightPackageView(APIView):
     serializer_class = FlightPackageSerializer
 
     def get(self, request, pk=None):
@@ -53,12 +64,7 @@ class FlightPackageView(APIView):
             serializer = FlightPackageSerializer(packages, many=True)
             return Response(serializer.data)
 
-    def post(self, request):
-        serializer = FlightPackageSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
     def put(self, request, pk):
         try:
